37aa912003792ef923935bebdc9122ef04e8454e
Vighnesh Shetty <117710817+vighnesh555@users.noreply.github.com>
2023-01-24 18:35:33 +0530

    Add files via upload

---
+++ Day5/mapper.r
@@ -0,0 +1,9 @@
+#!/usr/bin/Rscript
+input = file('stdin', open ='r')
+while (length(line <- readLines(input, n =1)) >0) {
+	words =unlist(strsplit(line, "[[:space:]]+"))
+	for (word in words)
+		cat(word, "\t1\n", sep='')
+}		
+close(input)		
+		
