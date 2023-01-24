16406a69b5964a1e94b1be0696b456da2fdafdd7
Vighnesh Shetty <117710817+vighnesh555@users.noreply.github.com>
2023-01-24 18:36:43 +0530

    Add files via upload

---
+++ Day6/funny_mapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+post= 0
+date= 0
+for line in sys.stdin:
+	words =line.split(",")
+	if words[9].isdigit() and post < int(words[9]):
+		post = int (words[9])
+		date = words[2]
+print('funny post', date, 'Total hahas: ', post)
